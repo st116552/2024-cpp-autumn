65a8aa53b352c170c0c4bb055a2a33673dc4b623
st116552 <st116552@student.spbu.ru>
2024-09-23 13:51:14 +0300

    add TextEditor

---
+++ 2024.09.23_HW1/main.cpp
@@ -0,0 +1,36 @@
+#include "TextEditor.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string input1 = "";
+    std::string input2 = "abcd";
+    std::string input3 = "efghi";
+    TextEditor te0 = TextEditor();
+    std::cout << te0 << '\n';
+    TextEditor te1 = TextEditor(input1);
+    std::cout << te1 << '\n';
+    TextEditor te2 = TextEditor(input2);
+    std::cout << te2 << '\n';
+    TextEditor te3 = TextEditor(input3);
+    std::cout << te3 << '\n';
+    te3 = te2;
+    std::cout << te3 << '\n';
+    te0.addText("112233");
+    std::cout << te0 << '\n';
+    std::string str = te0.cursorLeft(3);
+    std::cout << te0 << " " << str << '\n';
+    str = te0.cursorRight(2);
+    std::cout << te0 << " " << str << '\n';
+    te0.deleteText(3);
+    std::cout << te0 << '\n';
+    str = te0.cursorLeft(1);
+    std::cout << te0 << " " << str << '\n';
+    te0.addText("44");
+    std::cout << te0 << '\n';
+    std::cout << "-----" << '\n';
+    
+    return 0;
+}
+
